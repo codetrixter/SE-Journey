56034b4ad4bbf8e69368a91e2ded3d5043115679
codetrixter <abhishek20893@gmail.com>
2025-04-04 19:12:42 +0530

    SDL-3

--- CPP_Concepts/Scope-Duration-Linkage/Global-Evil.cpp
+++ CPP_Concepts/Scope-Duration-Linkage/Global-Evil.cpp
@@ -1,5 +1,5 @@
 /**
- * *Non-Const global variables are evil :
+ * * Non-Const global variables are evil :
  * ! - Non const. varibales are discouraged because due to their nature i.e external linkage, meaning any function of any translation unit can change the 
  * !value of that varibales in a way that make it difficult to debug -> This can be rectified by using local variables
  * ! - Initialization order problem of global variables
---
+++ CPP_Concepts/Scope-Duration-Linkage/topics.md
@@ -0,0 +1,10 @@
+# SCOPE, DURATION AND LINKAGE
+
+1. [Compound-Statements]
+2. [Namespaces]
+3. [Local-Variables]
+4. [Global-Variables]
+5. [Varibale-Shadowing]
+6. [Internal-Linkage]
+7. [External-Linkage]
+8. [Global-Varibales-Are-Evil]
