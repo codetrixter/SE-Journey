069c0b50bd23e0cfe5e030d684677c9248eb5788
codetrixter <abhishek20893@gmail.com>
2026-01-25 06:29:25 +0000

    sorting quick

--- NeetCode150/Problem-solving/Arrays-Hashing/sortArray.cpp
+++ NeetCode150/Problem-solving/Arrays-Hashing/sortArray.cpp
@@ -24,12 +24,70 @@ Constraints:
 
 using namespace std;
 
+// we are goig to use quick sort for sorting the array since it has O(n*logn) complexity.
+
+void swap(int &a, int &b)
+{
+    int temp = 0;
+    temp = b;
+    b = a;
+    a = temp;
+}
+
+int partition(vector<int> &A, int l, int h)
+{
+    int i = l, j = h, pivot = A[l];
+
+    while (i < j)
+    {
+        while (A[i] <= pivot)
+            i++;
+
+        while (A[j] > pivot)
+            j--;
+
+        if (i < j)
+            swap(A[i], A[j]);
+    }
+
+    // Moving pivot element to the sorted location.
+    swap(A[l], A[j]);
+
+    return j;
+}
+
+void quicksort(vector<int> &A, int l, int h)
+{
+    int partition_position = 0;
+    if (l < h)
+    {
+        partition_position = partition(A, l, h);
+        // will sort the element of all left most elements of partition position
+        quicksort(A, l, partition_position - 1);
+        // will sort the elemenst of right most elements of partition position
+        quicksort(A, (partition_position + 1), h);
+    }
+}
+
 vector<int> sortArray(vector<int> &input)
 {
+    input.push_back(__INT32_MAX__);
+
+    quicksort(input, 0, input.size() - 1);
+
+    input.pop_back();
+    return input;
 }
 
 int main(int argc, char const *argv[])
 {
+    vector<int> v = {10, 9, 1, 1, 1, 2, 3, 1};
+    auto res = sortArray(v);
+
+    for (auto &i : v)
+    {
+        cout << i << " ";
+    }
 
     return 0;
 }
